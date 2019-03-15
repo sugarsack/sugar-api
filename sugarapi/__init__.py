a11825d0b1234e25ac5bf78abe9a21e33d9d751b
Bo Maryniuk <bo@suse.de>
2019-03-15 23:31:22 +0100

    Initial implementation of the IPC master reference object

--- sugarapi/__init__.py
+++ sugarapi/__init__.py
@@ -1,6 +1,26 @@
 # coding: utf-8
 
 from fastapi import FastAPI
+from sugar.utils.objects import Singleton
+
 
 apirouter = FastAPI(title="Sugar API", version="0.0.0 Alpha")
 
+
+@Singleton
+class MasterRef:
+    """
+    Master reference object.
+    """
+    def __init__(self, channel=None):
+        self.receiver, self.sender = channel if channel else (None, None)
+
+    def get(self, property):
+        """
+        Get an attribute.
+
+        :param property:
+        :return:
+        """
+        self.sender.send(property)
+        return self.sender.recv()
