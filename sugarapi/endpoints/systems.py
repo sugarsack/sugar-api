35041bfeaca6d46191b204bc3cab03fba81bda55
Bo Maryniuk <bo@suse.de>
2019-03-16 22:02:25 +0100

    Add an example of the endpoint that takes all currently online target machines (clients).

--- sugarapi/endpoints/systems.py
+++ sugarapi/endpoints/systems.py
@@ -3,9 +3,17 @@
 API for the systems.
 """
 from fastapi import APIRouter
+from sugarapi import MasterRef
+
+
 apirouter = APIRouter()
 
 
 @apirouter.get("/systems")
 def get_systems():
     return {"systems": ["one", "two", "three"]}
+
+
+@apirouter.get("/systems/master")
+def get_ref():
+    return {"peers": MasterRef().ref.core.peer_registry.get_targets(":a")}
