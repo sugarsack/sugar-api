ef7511b586a004904b06b4b9f9f32bf9073e7413
Bo Maryniuk <bo@suse.de>
2019-03-17 18:54:11 +0100

    Implement query systems and all registered systems API methods

--- sugarapi/endpoints/systems.py
+++ sugarapi/endpoints/systems.py
@@ -3,17 +3,27 @@
 API for the systems.
 """
 from fastapi import APIRouter
-from sugarapi import MasterRef
+from sugarapi import get_master
 
 
 apirouter = APIRouter()
 
 
-@apirouter.get("/systems")
-def get_systems():
-    return {"systems": ["one", "two", "three"]}
+@apirouter.get("/clients/online")
+def query_systems(query=None):
+    """
+    Query systems. If query is omitting, all online systems are returned.
+    """
+    return {
+        "systems": get_master().ref.core.peer_registry.get_targets(query or "*")
+    }
 
 
-@apirouter.get("/systems/master")
-def get_ref():
-    return {"peers": MasterRef().ref.core.peer_registry.get_targets(":a")}
+@apirouter.get("/clients/all")
+def get_all_systems():
+    """
+    Return all systems with p-data, including offline.
+    """
+    return {
+        "systems": list(get_master().pdata_store.clients())
+    }
