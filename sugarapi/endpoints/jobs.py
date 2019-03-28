4c0035469d6a61491ae0e7594dc365c017fb221e
Bo Maryniuk <bo@suse.de>
2019-03-28 21:54:12 +0100

    Implement API endpoint for getting jobs overview

--- sugarapi/endpoints/jobs.py
+++ sugarapi/endpoints/jobs.py
@@ -1,1 +1,27 @@
 # coding: utf-8
+"""
+API for managing/viewing jobs.
+"""
+
+from fastapi import APIRouter
+from sugarapi import get_master
+from sugar.transport.serialisable import ObjectGate
+
+
+apirouter = APIRouter()
+
+
+@apirouter.get("/job/all")
+def get_all_jobs(limit: int = 25, offset: int = 0) -> dict:
+    """
+    Get all known jobs.
+
+    :param limit: limit per page
+    :param offset: offset of the page
+
+    :returns: jobs structure
+    """
+    return {
+        "jobs": [ObjectGate(job).to_dict()
+                 for job in get_master().job_store.get_all_overview(limit=limit, offset=offset)]
+    }
